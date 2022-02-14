879d9a5532be3a1ad886b2b3f87dcc261061e653
jenny00513 <63963832+jenny00513@users.noreply.github.com>
2022-02-15 01:52:18 +0900

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,106 @@
+// Template made for Korean programming
+// Created by: jenny00513, Github: github.com/jenny00513/hangul
+
+// 입출력
+#define 입력(x) cin >> x
+#define 출력(x) cout << x
+
+// 기본 구문
+#define 만약 if
+#define 아니라면 else
+#define 반복 for
+#define 조건을만족하는동안 while
+#define 그리고 &&
+#define 또는 ||
+#define 중단 break
+#define 하위중단 continue
+
+// 비트 연산
+#define 아니다 !
+#define 택 ^
+/* To do: and, or, <<, >> */
+
+// 템플릿
+#define 변수값까지순회(i, n) for(int i = 1; i <= n; i++)
+#define 전부(x) (x).begin(), (x).end()
+#define 압축(x) 정렬(전부((x))); (x).erase(unique(all((x))), (x).end())
+#define 크기(x) (int)((x).size())
+
+// 자료형
+#define 상수로 const
+#define 정수 int
+#define 없음 void
+#define 전구 bool
+#define 문자 char
+#define 실수 double
+#define 긴실수 long double
+#define 문자열 string
+#define 긴정수 long long
+#define 정수쌍 pair<int, int>
+#define 긴정수쌍 pair<long long, long long>
+#define 몰루 auto
+
+#define 첫번째값 first
+#define 두번째값 second
+
+// STL(standard template library)
+#define 동적배열 vector
+#define 집합 set
+#define 다중집합 multiset
+#define 사전 map
+#define 다중사전 multimap
+#define 해싱집합 unordered_set
+#define 해싱사전 unordered_map
+#define 우선순위큐 priority_queue
+#define 더미 stack
+#define 대기열 queue
+#define 양방향대기열 deque
+
+#define 추가 push
+#define 제거 pop
+
+#define 앞에추가 push_front
+#define 뒤에추가 push_back
+#define 앞에제거 pop_front
+#define 뒤에제거 pop_back
+
+#define 삽입 insert
+#define 삭제 erase
+
+#define 찾기 find
+#define 개수 count
+
+#define 더크다 greater
+
+#define 시작 begin
+#define 끝 end
+#define 시작의반대 rbegin
+
+#define 최대 max
+#define 최소 min
+#define 정렬 sort
+#define 하계 lower_bound
+#define 상계 upper_bound
+ 
+// 상수
+#define 참 true
+#define 거짓 false
+#define 아무것도아님 NULL
+#define 공백 ' '
+#define 개행 '\n'
+ 
+// 종료
+#define 종료 return 0
+#define 반환 return
+
+// 입출력 관련 템플릿
+#define 빠른입출력 ios_base::sync_with_stdio(거짓); cin.tie(아무것도아님); cout.tie(아무것도아님); cout << fixed; cout.precision(7)
+
+// 기타
+#define 구구팔 998244353
+#define 십억 1e9
+#define 십억칠 1e9+7
+#define 십억구 1e9+9
+#define 십팔억 1e18
+
+// Template end
